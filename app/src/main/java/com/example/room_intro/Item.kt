f46cdbf034c8d3a4c62d977078c2da2c0e1edade
David Svensson <davidsvson@gmail.com>
2022-11-02 07:43:17 +0100

    first commit

---
+++ app/src/main/java/com/example/room_intro/Item.kt
@@ -0,0 +1,12 @@
+package com.example.room_intro
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "item_table")
+data class Item (@PrimaryKey(autoGenerate = true) val id: Int,
+            @ColumnInfo(name = "name") var name: String?,
+            @ColumnInfo(name = "done") var done: Boolean = false,
+            @ColumnInfo(name = "category") var category : String?
+)
